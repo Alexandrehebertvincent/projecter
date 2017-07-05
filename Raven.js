89d1d56d6be11e6792d8e91559767d7ff5fc03a4
Alexandre Hébert-Vincent <alexandre.hebertvincent@gmail.com>
2017-07-05 13:38:05 -0400

    Update Raven.js

--- Raven.js
+++ Raven.js
@@ -1,3 +1,3 @@
-import Raven from 'raven';
+import Raven from 'raven'
 
-Raven.config('https://dd0b50e6f7ae4e0bb485b0e54e7d336a@sentry.io/188083').install();
+Raven.config('https://dd0b50e6f7ae4e0bb485b0e54e7d336a@sentry.io/188083').install()
