4afbd6a0025f8e9137d9be174af243f34e5cdf11
Alexandrehebertvincent <alexandre.hebertvincent@gmail.com>
2017-07-08 00:58:11 -0400

    Tests fixed

--- src/routes/Home/components/HomeView.js
+++ src/routes/Home/components/HomeView.js
@@ -4,25 +4,27 @@ import { Card, CardHeader, CardMedia, CardTitle, CardText } from 'material-ui/Ca
 import './HomeView.scss'
 
 export const HomeView = () => (
-  <Card>
-    <CardHeader
-      title='Projecter'
-      subtitle='..duck'
-      avatar={DuckImage}
-    />
-    <CardMedia
-      overlay={<CardTitle title='Hello there!' subtitle='Quack quack' />}
-    >
-      <img src={DuckImage} alt='Duck :)' />
-    </CardMedia>
-    <CardTitle title='Latin latin latin' subtitle='...' />
-    <CardText>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-      Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
-      Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
-      Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
-    </CardText>
-  </Card>
+  <div>
+    <Card>
+      <CardHeader
+        title='Projecter'
+        subtitle='..duck'
+        avatar={DuckImage}
+      />
+      <CardMedia
+        overlay={<CardTitle title='Hello there!' subtitle='Quack quack' />}
+      >
+        <img src={DuckImage} alt='Duck :)' />
+      </CardMedia>
+      <CardTitle title='Latin latin latin' subtitle='...' />
+      <CardText>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
+        Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
+        Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
+      </CardText>
+    </Card>
+  </div>
 )
 
 export default HomeView
